26567c6539b6a992cdf848e4c5b5399b3f1a358c
Enrique <octepc.97@gmail.com>
2022-08-19 11:32:37 -0500

    reviewed tuples. It gives to arrays a strong typing

---
+++ src/02-tuplas.ts
@@ -0,0 +1,8 @@
+const prices: (number | string)[] = [1,2,3,4,5,"hola"]
+prices.push(2)
+prices.push("como estás")
+
+let user: [string, number] = ['enri', 123]
+user = ['Diana', 12];
+user = ['pocha', 2]
+
